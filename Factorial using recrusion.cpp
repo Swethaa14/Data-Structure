05a986a6971ee492421d6798cc8460d7acac7e00
Swethaa14 <155937832+Swethaa14@users.noreply.github.com>
2024-05-13 08:50:55 +0530

    Add files via upload

---
+++ Factorial using recrusion.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int factorial(int n);
+
+int main() {
+    int number;
+    printf("Enter a positive integer: ");
+    scanf("%d", &number);
+    printf("Factorial of %d = %d", number, factorial(number));
+    return 0;
+}
+
+int factorial(int n) {
+    if (n == 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
+}
+
